c8b80ea75901941e479f3db78dc4e9dd5001c1fc
Elvis Pham <epha1206@gmail.com>
2018-04-27 17:46:30 -0400

    inventory update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,3 +27,68 @@ function validateInput(value) {
     }
 }
 
+//Prompt user for item and quantity they would like to purchase
+function promptUserPurchase() {
+
+    //Item selection prompt
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Please enter the item Id of the item you would like to purchase.',
+            validate: validateInput,
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'stock_quantity',
+            message: 'How many would you like?',
+            filter: Number
+        }
+    ]).then(function(input) {
+            var item = input.item_id;
+            var quantity = input.stock_quantity;
+
+            //query database to confirm item id and quantity
+            var queryStr = 'SELECT * FROM products WHERE ?';
+
+            connection.query(queryStr, {item_id: item}, function(err, data) {
+                if (err) throw err;
+
+                if (data.length === 0) {
+                    console.log('ERROR: Invalid item ID. Please select a valid ID.');
+                    displayInventory();
+                
+                } else {
+                    var productData = data[0];
+
+                    //if quantity selected is available
+                    if (quantity <= productData.stock_quantity) {
+                        console.log('We have item in stock. Thank you for placing order!');
+
+                        //updating query string
+                        var updateQueryStr = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + 'WHERE item_id = ' + item;
+
+                        //Update inventory
+                        connection.query(updateQueryStr, function(err, data) {
+                            if (err) throw err;
+
+                            console.log('Your order has been placed. Your total is $' + productData.product_price * quantity);
+                            console.log('Thank you for shopping with us!');
+                            console.log("\n-----------------------------------------------\n");
+
+                            //End database connection
+                            connection.end();
+                        })
+                    } else {
+                        console.log('Sorry, there is no more of this item in stock');
+                        console.log('Please adjust your order.');
+                        console.log("\n-----------------------------------------------\n");
+
+                        displayInventory();
+                    };
+                };
+            });
+    });
+};
+
