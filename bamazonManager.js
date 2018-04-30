6e59dc805c60833355d8f6150584ad3b6d4f8293
Elvis Pham <epha1206@gmail.com>
2018-04-30 16:11:17 -0400

    able to display inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -36,6 +36,50 @@ function promptManagerAction() {
             }
         }
     ]).then(function(input) {
-        
+
+        //sets off the correct option based on what the user inputs
+        if (input.option === 'sale') {
+            displayInventory();
+        } else if (input.option === 'lowInventory') {
+            displayLowInventory();
+        } else if (input.option === 'addInventory') {
+            addInventory();
+        } else if (input.option === 'newProduct') {
+            createNewProduct();
+        } else {
+            console.log('ERROR: Unsupported operation!');
+            exit(1);
+        }
+    })
+}
+
+//Function will retrieve inventory from database and output it to console
+function displayInventory() {
+
+    //construct db query string
+    queryStr = 'SELECT * FROM products';
+
+    //make the query
+    connection.query(queryStr, function(err, data) {
+        if (err) throw err;
+
+        console.log('Existing Inventory: ');
+        console.log('...................\n');
+
+        var strOut = '';
+        for (var i = 0; i < data.length; i++) {
+            strOut = '';
+            strOut += 'Item ID: ' + data[i].item_id + ' // ';
+            strOut += 'Product Name: ' + data[i].product_name + ' // ';
+            strOut += 'Department: ' + data[i].department_name + ' // ';
+            strOut += 'Price: $' + data[i].product_price + ' // ';
+            strOut += 'Quantity: ' + data[i].stock_quantity + '\n';
+
+            console.log(strOut);
+        }
+        console.log("-----------------------------------------------\n");
+
+        //End connection
+        connection.end();
     })
 }
